dff7e9913f400f111428e83d0ee619b3a1bdacff
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-09-02 23:55:25 +0530

    Learn about different types of chunking strategies.

---
+++ Week_4/day19_chunking_strategies/chunking.py
@@ -0,0 +1,5 @@
+from groq import Groq
+from dotenv import load_dotenv
+
+import numpy as np
+
---
+++ Week_4/day19_chunking_strategies/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from day19-chunking-strategies!")
+
+
+if __name__ == "__main__":
+    main()
